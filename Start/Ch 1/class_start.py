7ee1c347a4e087d2210cbb5db688695a08a48629
jhn316 <nissanka_w@yahoo.com>
2024-03-05 12:36:09 +0000

    chpt1 lessons

--- Start/Ch 1/class_start.py
+++ Start/Ch 1/class_start.py
@@ -4,26 +4,43 @@
 
 class Book:
     # TODO: Properties defined at the class level are shared by all instances
-
+    BOOK_TYPES = ("HARDCOVER", "PAPERBACK","EBOOK")
     # TODO: double-underscore properties are hidden from other classes
+    __booklist = None
 
-    # TODO: create a class method
+    # TODO: create a class method - unlike object methods these get CLASS as first argument
+    @classmethod
+    def get_book_types(cls):
+        return cls.BOOK_TYPES
 
-    # TODO: create a static method
+    # TODO: create a static method - takes no arguments
+    def get_book_list():
+        if Book.__booklist == None:
+            Book.__booklist = []
+        return Book.__booklist
 
     # instance methods receive a specific object instance as an argument
     # and operate on data specific to that object instance
     def set_title(self, newtitle):
         self.title = newtitle
 
-    def __init__(self, title):
+    def __init__(self, title,booktype):
         self.title = title
-
+        if (not booktype in Book.BOOK_TYPES):
+            raise ValueError(f"{booktype} is not a valid book type")
+        else:
+            self.booktype = booktype 
 
 # TODO: access the class attribute
-
+print("Book types: ", Book.get_book_types())
 
 # TODO: Create some book instances
-
+b1 = Book("Title1", "HARDCOVER")
+#b2 = Book("Title2", "COMIC")
+b3 = Book("Title3", "PAPERBACK")
 
 # TODO: Use the static method to access a singleton object
+thebooks = Book.get_book_list()
+thebooks.append(b1)
+thebooks.append(b3)
+print(thebooks) # note that we ensured that there will be only one of these lists
--- Start/Ch 1/definition_start.py
+++ Start/Ch 1/definition_start.py
@@ -3,9 +3,15 @@
 
 
 # TODO: create a basic class
-
+class Book():
+  #pass
+  def __init__(self, title) -> None:
+    self.title = title
 
 # TODO: create instances of the class
-
+book1 = Book("Brave new world")
+book2 = Book("War and Peace")
 
 # TODO: print the class and property
+print(book1)
+print(book1.title)
